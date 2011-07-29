a9f8a11b3095fde287fc2a4208c66d39c4d910d9
Alexandre Lymberopoulos <lymber@ime.usp.br>
2011-07-29 17:53:47 -0300

    Fixed type or arguments.
    Changed 'char *arquivo' to 'const char *arquivo' in constructor to
    prevent the "deprecated conversion from string constant to char* "
    error.

--- objeto.cpp
+++ objeto.cpp
@@ -13,12 +13,12 @@ Objeto *Objeto::Procura(int quem)
 {
    list<void *>::Iterator lista;
    Objeto *temp = (Objeto *) central->PrimeiroObjeto(&lista);
-   
+
    while (temp != NULL && temp->Mensagem(QUEM_VOCE, NULL) != quem)
    {
       temp = (Objeto *) central->ProximoObjeto(&lista);
    }
-   
+
    return temp;
 }
 
@@ -88,8 +88,8 @@ void Objeto::BateuNeste(Objeto *outro)
 bool Objeto::Bateu(Objeto *outro)
 {
    return (Box().intersectsWithBox(outro->Box()));
-   
-   //outro->Seletor()->getTriangles(temp, 10, quantos, box, NULL);  
+
+   //outro->Seletor()->getTriangles(temp, 10, quantos, box, NULL);
 }
 
 //--------------------------------------------------------------------------
@@ -120,7 +120,7 @@ vector3df Objeto::VetorCimaReal(void)
 //----------------------------------------------------------------------------
 vector3df Objeto::VetorFrente(void)
 {
-   vector3df v = vector3df(0, 0, -1);   
+   vector3df v = vector3df(0, 0, -1);
    matrix4 m;
    m.setRotationDegrees(vector3df(0, anguloHorizontal, 0));
    m.rotateVect(v);
@@ -130,7 +130,7 @@ vector3df Objeto::VetorFrente(void)
 //----------------------------------------------------------------------------
 vector3df Objeto::VetorTras(void)
 {
-   vector3df v = vector3df(0, 0, 1);   
+   vector3df v = vector3df(0, 0, 1);
    matrix4 m;
    m.setRotationDegrees(vector3df(0, anguloHorizontal, 0));
    m.rotateVect(v);
@@ -140,7 +140,7 @@ vector3df Objeto::VetorTras(void)
 //----------------------------------------------------------------------------
 vector3df Objeto::VetorEsquerda(void)
 {
-   vector3df v = vector3df(1, 0, 0);   
+   vector3df v = vector3df(1, 0, 0);
    matrix4 m;
    m.setRotationDegrees(vector3df(0, anguloHorizontal, 0));
    m.rotateVect(v);
@@ -150,7 +150,7 @@ vector3df Objeto::VetorEsquerda(void)
 //----------------------------------------------------------------------------
 vector3df Objeto::VetorDireita(void)
 {
-   vector3df v = vector3df(-1, 0, 0);   
+   vector3df v = vector3df(-1, 0, 0);
    matrix4 m;
    m.setRotationDegrees(vector3df(0, anguloHorizontal, 0));
    m.rotateVect(v);
@@ -162,7 +162,7 @@ vector3df Objeto::VetorDireita(void)
 bool Objeto::NaFrente(vector3df outro)
 {
    vector3df v = outro - Posicao();
-   
+
    return (VetorFrente().dotProduct(v) >= 0);
 }
 
@@ -171,7 +171,7 @@ bool Objeto::NaFrente(vector3df outro)
 bool Objeto::NaTras(vector3df outro)
 {
    vector3df v = outro - Posicao();
-   
+
    return (VetorFrente().dotProduct(v) < 0);
 }
 
@@ -180,7 +180,7 @@ bool Objeto::NaTras(vector3df outro)
 bool Objeto::NaEsquerda(vector3df outro)
 {
    vector3df v = outro - Posicao();
-   
+
    return (VetorEsquerda().dotProduct(v) >= 0);
 }
 
@@ -190,7 +190,7 @@ bool Objeto::NaEsquerda(vector3df outro)
 bool Objeto::NaDireita(vector3df outro)
 {
    vector3df v = outro - Posicao();
-   
+
    return (VetorDireita().dotProduct(v) >= 0);
 }
 
@@ -204,7 +204,7 @@ void Objeto::AdicionaCenario(ISceneNode *cenarioAux)
    // crio uma elipse em volta da Objeto para facilitar na colisão
    core::aabbox3d<f32> box = node->getBoundingBox();
    core::vector3df radius = box.MaxEdge - box.getCenter();
-	      
+
    // crio um animador que, para este caso só não deixa a Objeto entrar nas paredes do cenário
    scene::ISceneNodeAnimator* anim =  central->Device()->getSceneManager()->createCollisionResponseAnimator(
 	   	cenario->getTriangleSelector(), node, radius,
@@ -217,8 +217,8 @@ void Objeto::AdicionaCenario(ISceneNode *cenarioAux)
 void Objeto::MudaPosicao(vector3df qual)
 {
    //keep things on the right plane
-   qual = vector3df(qual.X, 0, qual.Z);   
-   
+   qual = vector3df(qual.X, 0, qual.Z);
+
    posicaoEstimada = qual;
    node->setPosition(qual);
 }
@@ -243,68 +243,68 @@ void Objeto::Atualiza(void)
    if (piscando > 0){
       piscando--;
    }
-   
-   
-  
+
+
+
    //node->setMaterialFlag(EMF_WIREFRAME, (piscando % 5) > 3);
    node->setMaterialFlag(EMF_WIREFRAME, piscando  != 0);
    //calculo momento
-      
+
    //ainda não atingiu a velocidade máxima
-   if (aceleracao >= 0 &&  momento.dotProduct(VetorFrente()) < velocidadeMaxima) 
+   if (aceleracao >= 0 &&  momento.dotProduct(VetorFrente()) < velocidadeMaxima)
          momento = momento + (aceleracao * VetorFrente());
-   if (aceleracao < 0 &&  momento.dotProduct(VetorFrente()) > -velocidadeMaxima) 
+   if (aceleracao < 0 &&  momento.dotProduct(VetorFrente()) > -velocidadeMaxima)
          momento = momento + (aceleracao * VetorFrente());
-   
+
    //if (momento.getLength() >= 2 * velocidadeMaxima) momento = (1 / momento.getLength()) * momento;
-   
+
    //vejo o que varia com o impacto
    momento = momento + momentoImpacto;
    momentoImpacto = vector3df(0, 0, 0);
    //não deixo o momento ser grande demais
-   if (momento.getLength() > 2) momento = (2 / momento.getLength()) * momento;      
+   if (momento.getLength() > 2) momento = (2 / momento.getLength()) * momento;
    //tiro a resistência (de maneira linear)
    if (momento.getLength() > 0)
    {
       if (momento.getLength() < resistencia) momento = vector3df(0, 0, 0);
       else momento = (1 - resistencia) * momento;
    }
-   
+
    //calculo a translação e verifico se bateu no cenário
    if (!posicaoEstimada.equals(node->getPosition(), 0.1f)) bateuCenario = true;
    else bateuCenario = false;
    posicaoEstimada = node->getPosition() + momento;
-   
-   
+
+
    /*list<void *>::Iterator lista;
    Objeto *outro = (Objeto *) central->PrimeiroObjeto(&lista);
-   
+
    while (outro != NULL)
    {
       if (!SouEu(outro) && Bateu(outro)) BateuNeste(outro);
       outro = (Objeto *) central->ProximoObjeto(&lista);
-   }*/ 
-   
-   
+   }*/
+
+
    //node->setPosition(posicaoEstimada);
    MudaPosicao(posicaoEstimada);
-   
+
    if (esq)
    {
-      if (quantoVira > -maxVirada) quantoVira -= velocidadeVirada;     
+      if (quantoVira > -maxVirada) quantoVira -= velocidadeVirada;
    }
    else
    {
       if (dir)
       {
-         if (quantoVira < maxVirada) quantoVira += velocidadeVirada;         
+         if (quantoVira < maxVirada) quantoVira += velocidadeVirada;
          //anguloHorizontal += 1;
          //inclinacao += 1;
          //if (inclinacao > maxInclinacao) inclinacao = maxInclinacao;
       }
       else{
       	if (quantoVira > 2*velocidadeVirada) quantoVira -= 2*velocidadeVirada;
-	else if (quantoVira < -2*velocidadeVirada) quantoVira += 2*velocidadeVirada; 
+	else if (quantoVira < -2*velocidadeVirada) quantoVira += 2*velocidadeVirada;
 	else quantoVira = 0;
 	if (inclinacao > 0) inclinacao -= 0.5f;
 	if (inclinacao < 0) inclinacao += 0.5f;
@@ -314,7 +314,7 @@ void Objeto::Atualiza(void)
    inclinacao += quantoVira;
    if (inclinacao < -maxInclinacao) inclinacao = -maxInclinacao;
    if (inclinacao > maxInclinacao) inclinacao = maxInclinacao;
-      
+
    //arrumo a rotação da nave
    node->setRotation(vector3df(0, anguloHorizontal, 0));
    matrix4 m;
@@ -324,7 +324,7 @@ void Objeto::Atualiza(void)
 }
 
 //-------------------------------------------------------------------------
-//faz a comunicação entre objetos. Como não entendo mensagens por enquanto, 
+//faz a comunicação entre objetos. Como não entendo mensagens por enquanto,
 //retorno NULL
 int Objeto::Mensagem(int msg, void *param)
 {
@@ -332,7 +332,7 @@ int Objeto::Mensagem(int msg, void *param)
 }
 
 //---------------------------------------------------------------------------
-Objeto::Objeto(Central *centralAux, char *nome)
+Objeto::Objeto(Central *centralAux, const char *nome)
 {
    central = centralAux;
    ZeraComandos();
@@ -358,7 +358,7 @@ Objeto::Objeto(Central *centralAux, char *nome)
 	//seletor = central->Device()->getSceneManager()->createTriangleSelector(mesh->getMesh(0), node);
 	//node->setTriangleSelector(seletor);
    //seletor->drop();
-	
+
 	piscando = TEMPO_PISCANDO;
 	momento = vector3df(0, 0, 0);
 	massa = MASSA_OBJETO;
